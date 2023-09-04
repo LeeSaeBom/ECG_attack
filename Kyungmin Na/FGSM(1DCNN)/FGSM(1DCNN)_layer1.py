24f3fc27cb4c5e839200c0c2cc9a9c984071d4e3
Kyung-Min Na <133650591+Kyung-Min-Na@users.noreply.github.com>
2023-09-04 21:09:07 +0900

    Update FGSM(1DCNN)_layer1.py

--- Kyungmin Na/FGSM(1DCNN)/FGSM(1DCNN)_layer1.py
+++ Kyungmin Na/FGSM(1DCNN)/FGSM(1DCNN)_layer1.py
@@ -123,7 +123,7 @@ for epsilon in epsilons:
         predicted_labels.extend(predicted.cpu().numpy())
 
     accuracy = accuracy_score(true_labels, predicted_labels)
-    f1 = f1_score(true_labels, predicted_labels, average='weighted')  # Update the average parameter to 'weighted'
+    f1 = f1_score(true_labels, predicted_labels, average='weighted')
     print(f"Epsilon: {epsilon}, Accuracy: {accuracy:.4f}%, F1-Score: {f1:.4f}")
     results_file.write(f"Epsilon: {epsilon}, Accuracy: {accuracy:.4f}%, F1-Score: {f1:.4f}\n")
 
